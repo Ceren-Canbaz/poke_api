0f658cd63fdb8139b92aa310adea53686b7ab981
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2023-03-01 12:31:12 +0300

    fix

--- lib/repository/repositories.dart
+++ lib/repository/repositories.dart
@@ -3,23 +3,29 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:poke_api/model/pokemon_model.dart';
-import 'package:poke_api/old/poke.dart';
 
 class PokemonRepository {
   String baseUrl = 'https://pokeapi.co/api/v2/pokemon';
-
-  Future<List<PokemonModel>> getPokemons() async {
+  List idArr = [];
+  Future<List<Pokemon>> getPokemons() async {
     http.Response response = await http.get(Uri.parse(baseUrl));
 
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body)['results'];
-      List<PokemonModel> list = result
-          .map<PokemonModel>((json) => PokemonModel.fromJson(json))
-          .toList();
+      List<Pokemon> list =
+          result.map<Pokemon>((json) => Pokemon.fromJson(json)).toList();
+      splitUrlforId(list);
       return list;
     } else {
       throw Exception(response.reasonPhrase);
     }
+  }
+
+  void splitUrlforId(List<Pokemon> list) {
+    list.forEach((element) {
+      idArr = element.url.split('/');
+      element.url = idArr[idArr.length - 2];
+    });
   }
 
   Future<PhotoModel> fetchPhoto(http.Client client,
